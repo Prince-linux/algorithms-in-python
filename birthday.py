3f4980633fabe4ce3cd22a030c97f77dae5360bb
Prince Boamah <regioths@gmail.com>
2022-12-29 06:04:18 +0000

    A simple age callculator

---
+++ birthday.py
@@ -0,0 +1,6 @@
+current_year = int(input("Please enter the current year: "))
+birth_year = int(input("Please enter the year of birth: "))
+current_age = current_year - birth_year
+next_age = current_age + 1 
+print(f"Your age is {current_age} year(s) old, you will be {next_age} year(s) old next year")
+
