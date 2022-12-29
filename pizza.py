408cb015274cdb649d4e801954042db2dc17ae17
Prince Boamah <regioths@gmail.com>
2022-12-29 06:04:55 +0000

    A simple pizza slice calculator

---
+++ pizza.py
@@ -0,0 +1,5 @@
+slices = 12
+slices_eaten = int(input("Please enter the slices already eaten: "))
+slices_left = slices - slices_eaten
+print(f"You've eaten {slices_eaten} slices, so the sices left to eat are/is {slices_left}")
+
