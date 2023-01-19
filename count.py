61c477aa54c6c14ed3fb788fbc1a1c16095e0a0b
Prince Boamah <regioths@gmail.com>
2023-01-19 04:33:27 +0000

    A simple count occurence program

---
+++ count.py
@@ -0,0 +1,9 @@
+# A python program that counts the number of occurrences of a particular number and adds it.
+# Author: Prince Oppong Boamah
+# Date: 18th January 2023
+
+num = [1,2,3,4,5,6,7,8,9,10,2,34,42,2,4,7,6,90,3,2]
+count_num = num.count(2) * 2
+print(count_num)
+
+
