cfe86c0a819947073bcbcd75b1757acba055462a
Prince Boamah <regioths@gmail.com>
2023-01-27 18:28:36 +0000

    Added a second rule to the game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,34 +5,54 @@
 import random
 
 item_list = {1: "rock", 2: "paper", 3: "scissors"}
-choice = int(input("Type in one of these choices (1:rock, 2:paper, 3:scissors): "))
-random_choice = random.choice(list(item_list.values()))
 
-print(f"CPU selection: {random_choice}")
+user_win = 0
+cpu_win = 0
+draw = 0
 
-if item_list[choice] == "rock":
-    if random_choice == "rock":
-        print("Draw!")
-    elif random_choice == "paper":
-        print("You Lose!")
-    elif random_choice == "scissors":
-        print("You Win!")
+for counter in range(0, 4):
+    if user_win == 2:
+        print(f"You are the winner of this game. you won {user_win} games")
+        break
 
-elif item_list[choice] == "paper":
-    if random_choice == "rock":
-        print("You Win!")
-    elif random_choice == "paper":
-        print("Draw!")
-    elif random_choice == "scissors":
-        print("You Lose!")
+    elif cpu_win == 2:
+        print(f"You lost {cpu_win} to CPU ")
+        break
 
-elif item_list[choice] == "scissors":
-    if random_choice == "rock":
-        print("You Lose!")
-    elif random_choice == "paper":
-        print("You Win!")
-    elif random_choice == "scissors":
-        print("Draw!")
-else:
-    print("You made the wrong choice! Game Over!!")
+    elif counter == 3:
+      print("The game is a draw")
+      break
+    try:
+      choice = int(input("Type in one of these choices (1:rock, 2:paper, 3:scissors): "))
+    except ValueError:
+      print("You did not enter a valid number")
+      break
+    random_choice = random.choice(list(item_list.values()))
+    print(f"CPU selection: {random_choice}")
+    if item_list[choice] == random_choice:
+       print("Draw!")
 
+    if item_list[choice] == "rock":  
+        if random_choice == "paper":
+            cpu_win += 1
+            print("You Lose!")
+        elif random_choice == "scissors":
+            user_win += 1
+            print("You Win!")
+
+    elif item_list[choice] == "paper":
+        if random_choice == "rock":
+            user_win += 1
+            print("You Win!")
+      
+        elif random_choice == "scissors":
+            cpu_win += 1
+            print("You Lose!")
+
+    elif item_list[choice] == "scissors":
+        if random_choice == "rock":
+            cpu_win += 1
+            print("You Lose!")
+        elif random_choice == "paper":
+            user_win += 1
+            print("You Win!")
